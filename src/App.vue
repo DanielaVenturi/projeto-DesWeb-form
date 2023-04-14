1e3ede478069dc97246cdf44d026b24618c33201
DanielaVenturi <danielaventuri14@gmail.com>
2023-04-14 12:29:28 -0300

    sss

--- src/App.vue
+++ src/App.vue
@@ -12,147 +12,178 @@ const bio = ref('')
 const lingPro = ref('')
 const enviar = ref(false)
 const senhaConf = ref('')
+
 </script>
 
 <template>
-  <div class="conteudo">
+  <div class="container">
 
-<div class="tudo informacoes">
+    <div class="box">
 
-  <form @submit.prevent='enviar = !enviar'>
+      <form @submit.prevent='enviar = !enviar'>
 
-<h1>Login</h1>
-<div>
-  <p>Nome</p>
+        <h1>Login</h1>
+        <label for="nome">Nome:</label>
+        <div class="margin">
+          
 
-  <input type="text" v-model="nome" placeholder="Digite seu nome" required />
+          <input name="nome" type="text" v-model="nome" placeholder="Digite seu nome" required />
 
-</div>
+        </div>
+        <label for="email">e-Mail:</label>
+        <div class="margin">
+          
+          <input name="email" type="email" v-model="email" placeholder="Digite um E-mail" required />
 
-<div>
-  <p>Email</p>
-  <input type="email" v-model="email" placeholder="Digite um E-mail" required />
-  
-</div>
+        </div>
+        <label for="senha">Senha:</label>
+        <div class="margin">
+          
+          <input neme="senha" type="password" v-model="senha" placeholder="Digite uma senha" required>
+          <br>
+          <input name="senha" type="password" v-model="senhaConf" placeholder="Confirme sua senha" required>
+        </div>
+        <label for="data">Data de Nascimento</label>
+        <div class="margin">
+          
+          <input name="data" type="date" v-model="data" required>
+        </div>
+        <label for="endereco">Endereço</label>
+        <div class="margin">
+         
+          <input name="endereco" type="text" v-model="rua" placeholder="Digite o Nome da Rua" required><br>
+          <input  name="endereco" type="text" v-model="bairro" placeholder="Digite o Nome da Bairro" required>
+          
+          <select name="endereco" v-model="estados">
+            <option value="">Estado</option>
+            <option value="Acre">AC</option>
+            <option value="Alagoas">AL</option>
+            <option value="Amapa">AP</option>
+            <option value="Amazonas">AM</option>
+            <option value="Bahia">BA</option>
+            <option value="Ceara">CE</option>
+            <option value="Distrito Federal">DF</option>
+            <option value="Espirito Santo">ES</option>
+            <option value="Goiania">GO</option>
+            <option value="Maranhao">MA</option>
+            <option value="Mato grosso">MT</option>
+            <option value="Mato gosso do sul">MS</option>
+            <option value="Minas gerais">MG</option>
+            <option value="Para">PA</option>
+            <option value="Paraiba">PB</option>
+            <option value="Parana">PR</option>
+            <option value="Pernambuco">PE</option>
+            <option value="Piaui">PI</option>
+            <option value="Rio de Janeiro">RJ</option>
+            <option value="Rio grande do Norte">RN</option>
+            <option value="Rio grande do sul">RS</option>
+            <option value="Rondonia">RO</option>
+            <option value="Roraima">RR</option>
+            <option value="Santa Catarina">SC</option>
+            <option value="Sao paulo">SP</option>
+            <option value="Sergipe">SE</option>
+            <option value="Tocantins">TO</option>
+          </select>
+          <input name="endereco" type="text" placeholder="Digite o nome da sua Cidade" required>
 
-<div>
-  <p>Senha</p>
-  <input type="password" v-model="senha" placeholder="Digite uma senha" required>
-  <br>
-  <input type="password" v-model="senhaConf" placeholder="Confirme sua senha" required>
-</div>
+        </div>
+        <label for="bio">Um Pouco Sobre Você</label>
+        <div class="margin">
+          
+          <input name="bio" type="text" v-model="hobies" placeholder="Digite um dos seus Hobbies" required maxlength="25"
+            minlength="10"><br>
 
-<div>
-  <p>Data de nascimento</p>
-  <input type="date" v-model="data" required>
-</div>
+          <input name="bio" type="text" v-model="bio" placeholder="Biografia curta" required maxlength="51" minlength="10"><br>
 
-<div>
-  <p>Endereço</p>
-  <input type="text" v-model="rua" placeholder="Rua" required>
-  <input type="text" v-model="bairro" placeholder="Bairro" required>
-  <br>
-  <select v-model="estados">
-    <option value="">Estado</option>
-    <option value="Acre">AC</option>
-    <option value="Alagoas">AL</option>
-    <option value="Amapa">AP</option>
-    <option value="Amazonas">AM</option>
-    <option value="Bahia">BA</option>
-    <option value="Ceara">CE</option>
-    <option value="Distrito Federal">DF</option>
-    <option value="Espirito Santo">ES</option>
-    <option value="Goiania">GO</option>
-    <option value="Maranhao">MA</option>
-    <option value="Mato grosso">MT</option>
-    <option value="Mato gosso do sul">MS</option>
-    <option value="Minas gerais">MG</option>
-    <option value="Para">PA</option>
-    <option value="Paraiba">PB</option>
-    <option value="Parana">PR</option>
-    <option value="Pernambuco">PE</option>
-    <option value="Piaui">PI</option>
-    <option value="Rio de Janeiro">RJ</option>
-    <option value="Rio grande do Norte">RN</option>
-    <option value="Rio grande do sul">RS</option>
-    <option value="Rondonia">RO</option>
-    <option value="Roraima">RR</option>
-    <option value="Santa Catarina">SC</option>
-    <option value="Sao paulo">SP</option>
-    <option value="Sergipe">SE</option>
-    <option value="Tocantins">TO</option>
-  </select>
-  <input type="text" placeholder="Digite o nome da sua Cidade" required>
+          <select neme="bio" v-model="lingPro">
+            <option value="">Linguagem De Programação</option>
+            <option value="JavaScript">JS</option>
+            <option value="Java">Java</option>
+            <option value="Python">Python</option>
+            <option value="PHP">PHP</option>
+            <option value="C++">C++</option>
+            <option value="C#">C#</option>
+            <option value="TypeScript">TypeScript</option>
+            <option value="Ruby">Ruby</option>
+          </select>
 
-</div>
-<div>
-  <p>Sobre Você</p>
-  <input type="text" v-model="hobies" placeholder="Digite um dos seus Hobbies" required maxlength="25"
-    minlength="10">
+         
+          
+        </div>
+        <br>
+        <button type='submit' class="btn btn-outline-primary">Cadastrar</button>
+      </form>
 
-  <input type="text" v-model="bio" placeholder="Biografia curta" required maxlength="51" minlength="10">
+    </div>
 
-  <select v-model="lingPro">
-    <option value="">Linguagem De Programação</option>
-    <option value="JavaScript">JS</option>
-    <option value="Java">Java</option>
-    <option value="Python">Python</option>
-    <option value="PHP">PHP</option>
-    <option value="C++">C++</option>
-    <option value="C#">C#</option>
-    <option value="TypeScript">TypeScript</option>
-    <option value="Ruby">Ruby</option>
-  </select>
-</div>
-<br>
-<button type='submit' class="btn btn-outline-primary" >Cadastrar</button>
-</form>
+    <div class="box" v-if="enviar">
 
-</div>
+     
+        <div>
+          <h1>Login</h1>
+        </div><br>
+        <div class="margin"> Nome: {{ nome }}</div>
+        <div class="margin"> Email: {{ email }}</div>
+        <div  class="margin">Senha: {{ senha }}</div>
+        <div  class="margin">Senha Confirmada: {{ senhaConf }}</div>
+        <div class="margin">
+          <h3>Data de Nascimento</h3>
+        </div>
+        <div  class="margin">Data: {{ data }}</div> 
+        <div class="margin">
+          <h3>Endereço</h3>
+        </div>
+        <div  class="margin">Rua: {{ rua }} </div>
+        <div  class="margin">Bairro: {{ bairro }}</div>
+        <div  class="margin">Estado: {{ estados }} </div>
+        <div class="margin">
+          <h3>Hobbies</h3>
+        </div>
+        <div  class="margin">Hobbies: {{ hobies }}</div>
+        <div  class="margin">Bio: {{ bio }}</div>
+        <div  class="margin"> Linguagem de Programação : {{ lingPro }}</div>
+        {{  }}
 
-<div v-if="enviar">
+     <div  class="margin">
 
-  <div class="tudo enviar">
+     </div>
 
-<div> Nome:   {{ nome }}</div>
-<div> Email:{{ email }}</div>
-<div>Senha:  {{ senha }}</div>
-<div>Senha Confirmada: {{ senhaConf }}</div>
-<div>Data: {{ data }}</div> 
-<div>Rua: {{ rua }} </div>
-<div>Bairro: {{ bairro }}</div>
-<div>Estado: {{ estados }} </div>
-<div>Hobbies: {{ hobies }}</div>
-<div>Bio: {{ bio }}</div>
-<div> Linguagem de Programação : {{ lingPro }}</div>
+    </div>
 
   </div>
-
-</div>
-
-</div>
 </template>
 
 <style scoped>
-.conteudo{
-  color: #fff;
-  text-align: center;
-  
-}
-
-
-.tudo{
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   background-color: darkgrey;
   border-style: outset;
+  height: 600px;
+  width: 600px;
+  margin: 0 auto;
+  margin-top: 150px;
   
-  width: 400px;
-  
-}
-.informacoes{
-
 }
 
-.enviar{
-font-weight: bold;
+.box {
+ text-align: center;
+ border-style: outset;
+  height: 600px;
+  width: 600px;
+  margin: 0 auto;
 }
+
+.margin{margin-top: 10px;
+margin-bottom: 10px;
+}
+
+
+
+
+
+
+
+
 </style>
